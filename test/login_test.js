e40e676a938baefcda2a2cea7d48e8ac4e83db74
Pablo Stenico Soares dos Santos <pablostenico.santos@gmail.com>
2022-08-25 22:02:23 -0300

    adicionando scroll no teste

--- test/login_test.js
+++ test/login_test.js
@@ -1,12 +1,25 @@
 Feature('login');
 const {I, login_page} = inject()
 
-Scenario('Login with sucess', ({home_page}) => {
-    I.wait(5)
+Scenario('Login with success', ({home_page}) => {
+    I.wait(2)
     login_page.doLogin('teste@teste.com','123456')
     home_page.checkLoginSucess()
+
+    I.touchPerform([
+    {
+        action: 'longPress',
+        options:{x:300, y:1100}
+    },
+    {
+            action: 'moveTo',
+            options:{x:300, y:250}  
+    },
+    { action: 'release' }
+
+])
  
-});
+}).tag('@login_success');
 
 Scenario('Login error', () => {
     I.wait(5)
